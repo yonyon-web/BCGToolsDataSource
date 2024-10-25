955778eb3f54c83b3651335b2acd27c2b33843bf
44 <44x1carbon@gmail.com>
2024-10-25 09:39:01 +0900

    Upload or update LastmemoriesWeapon.d.ts via Add-on

---
+++ LastmemoriesWeapon.d.ts
@@ -0,0 +1,21 @@
+export namespace Lastmemories {
+  export type Weapon = {
+    id: string;
+    name: string;
+    role: string;
+    hp: string;
+    mp: string;
+    pAtk: string;
+    mAtk: string;
+    hMag: string;
+    pDef: string;
+    mDef: string;
+    acc: string;
+    eva: string;
+    crit: string;
+    agi: string;
+    skill4: string;
+    skill5: string;
+    skill6: string;
+  }
+}
