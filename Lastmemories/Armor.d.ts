a07c623e4b5b2aabe74c6a4327d338f9c6525587
44 <44x1carbon@gmail.com>
2024-11-01 12:49:19 +0900

    Upload or update Lastmemories/Armor.d.ts via Add-on

---
+++ Lastmemories/Armor.d.ts
@@ -0,0 +1,19 @@
+export type Armor = {
+    id: string;
+    name: string;
+    role: string;
+    part: string;
+    back: string;
+    setEffect: string;
+    hp: number;
+    mp: number;
+    pAtk: number;
+    mAtk: number;
+    hMag: number;
+    pDef: number;
+    mDef: number;
+    acc: number;
+    eva: number;
+    crit: number;
+    agi: number;
+}
