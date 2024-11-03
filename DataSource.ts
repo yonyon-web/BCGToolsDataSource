ef62532ce6c4fc0e3956c8b96e71e83437f3ebb8
44 <44x1carbon@gmail.com>
2024-11-03 17:06:13 +0900

    .

--- DataSource.ts
+++ DataSource.ts
@@ -9,16 +9,16 @@ export class DataSource<T> {
         this.data = data;
     }
 
-    toArray<K extends keyof T>(key: K, separator: string = ","): DataSource<ReplaceProperty<T, K, string[]>> {
+    toArray<K extends keyof T, R extends ReplaceProperty<T, K, string[]>>(key: K, separator: string = ","): DataSource<R> {
         const newData = this.data.map(item => {
-            // @ts-ignore
-            const array = item[key].split(separator);
+            const array = String(item[key]).split(separator).filter(s => s);
 
             return {
                 ...item,
                 [key]: array
             }
-        })
+        }) as unknown as R[]
+
         return new DataSource(newData);
     }
 
@@ -67,7 +67,7 @@ export class DataSource<T> {
             return {
                 ...item,
                 [`_${String(key)}`]: item[key],
-                [key]: other.values.find(oItem => oItem[oKey].toString().trim() === String(item[key]).trim()) as O
+                [key]: other.data.find(oItem => oItem[oKey].toString().trim() === String(item[key]).trim()) as O
 
             }
         }) as unknown as R[];
@@ -75,7 +75,4 @@ export class DataSource<T> {
         return new DataSource(newData);
     }
 
-    get values(): T[] {
-        return this.data;
-    }
 }
