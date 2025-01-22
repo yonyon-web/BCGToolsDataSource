53cb66b2197e0e6173060c37a053831760c45f07
44 <44x1carbon@gmail.com>
2025-01-22 11:31:39 +0900

    Upload or update Lastmemories/SubStatus.d.ts via Add-on

---
+++ Lastmemories/SubStatus.d.ts
@@ -0,0 +1,20 @@
+export type SubStatus = {
+    id: string;
+    name: string;
+    effect: string;
+    a1: number;
+    a2: number;
+    a3: number;
+    a4: number;
+    a5: number;
+    b1: number;
+    b2: number;
+    b3: number;
+    b4: number;
+    b5: number;
+    c1: number;
+    c2: number;
+    c3: number;
+    c4: number;
+    c5: number;
+}
